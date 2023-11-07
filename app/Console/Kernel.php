a922fafd75c8938f14403b8a62e7ae6c3893daaf
= <=>
2023-11-07 13:47:24 +0700

    devkh

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        // clean backup
+        $schedule->command('backup:clean')->daily()->at('01:00');
+        // Backing up database
+        $schedule->command('backup:run')->daily()->at('01:30');
     }
 
     /**
