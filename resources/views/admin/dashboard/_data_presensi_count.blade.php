7e2f4165732519206e592dae8201757a19b463b4
= <=>
2024-01-09 17:24:10 +0700

    devkh

--- resources/views/admin/dashboard/_data_presensi_count.blade.php
+++ resources/views/admin/dashboard/_data_presensi_count.blade.php
@@ -1,19 +1,34 @@
-@foreach ($table as $tb)
-    <div class="demo-vertical-spacing">
-      <a href="/admin/presensi/opd/show/{{ $tb->opd_id }}">
-        <div class="text-light small fw-semibold mb-1">{{ $tb->opd->nama_opd }}</div>
-        <div class="progress mb-3">
-            <div
-                class="progress-bar"
-                role="progressbar"
-                style="width: {{ round($tb->total_presensi / $tb->total_user * 100) }}%"
-                aria-valuenow="{{ round($tb->total_presensi / $tb->total_user * 100) }}"
-                aria-valuemin="0"
-                aria-valuemax="100"
-            >
-                {{ round($tb->total_presensi / $tb->total_user * 100) }}%
-            </div>
+@forelse ($table as $tb)
+<div class="demo-vertical-spacing">
+  <a href="/admin/presensi/opd/show/{{ $tb->opd_id }}">
+    <div class="text-light small fw-semibold mb-1">{{ $tb->opd->nama_opd }}</div>
+    <div class="progress mb-3">
+        <div
+            class="progress-bar"
+            role="progressbar"
+            style="width: {{ round($tb->total_presensi / $tb->total_user * 100) }}%"
+            aria-valuenow="{{ round($tb->total_presensi / $tb->total_user * 100) }}"
+            aria-valuemin="0"
+            aria-valuemax="100"
+        >
+            {{ round($tb->total_presensi / $tb->total_user * 100) }}%
         </div>
-      </a>
     </div>
-@endforeach
+  </a>
+</div>
+@empty
+<div class="col text-center">
+  <div class="empty-img">
+      <svg  style="width: 96px; height: 96px" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-database-off" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M12.983 8.978c3.955 -.182 7.017 -1.446 7.017 -2.978c0 -1.657 -3.582 -3 -8 -3c-1.661 0 -3.204 .19 -4.483 .515m-2.783 1.228c-.471 .382 -.734 .808 -.734 1.257c0 1.22 1.944 2.271 4.734 2.74"></path>
+          <path d="M4 6v6c0 1.657 3.582 3 8 3c.986 0 1.93 -.067 2.802 -.19m3.187 -.82c1.251 -.53 2.011 -1.228 2.011 -1.99v-6"></path>
+          <path d="M4 12v6c0 1.657 3.582 3 8 3c3.217 0 5.991 -.712 7.261 -1.74m.739 -3.26v-4"></path>
+          <line x1="3" y1="3" x2="21" y2="21"></line>
+      </svg>
+  </div>
+  <p class="empty-title">Tidak ada data ditemukan!</p>
+</div>
+@endforelse
+
+
