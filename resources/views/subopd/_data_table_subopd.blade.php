92c3b196d702c609c3030c597a6feaac6710d8c0
= <=>
2024-02-22 11:43:38 +0700

    devkh

--- resources/views/subopd/_data_table_subopd.blade.php
+++ resources/views/subopd/_data_table_subopd.blade.php
@@ -1,28 +1,12 @@
-{{-- @forelse ($table as $key => $tb) --}}
-    <div class="card mt-2">
+@forelse ($table as $key => $tb)
         <a href="javascript:void(0)"
-        onclick="openCamera(4, -4.3011819, 105.2201634)">
-        <div class="card-body">
-            <p>1. MPP Mini Tulang Bawang</p>
-        </div>
-    </a>
-</div>
-    <div class="card mt-2">
-        <a href="javascript:void(0)"
-            onclick="openCamera(4, -5.3773215, 105.2790887)">
-            <div class="card-body">
-                <p>2. Anjungan Tulang Bawang</p>
-            </div>
-        </a>
-    </div>
-        {{-- <a href="javascript:void(0)"
             onclick="openCamera(4, {{ $tb->lat }}, {{ $tb->long }})">
             <div class="card-body">
                 <p>{{ $key + 1 . '. ' . $tb->nama_sub_opd }}</p>
             </div>
-        </a> --}}
-{{-- @empty --}}
-    {{-- <div class="mt-2 text-center">
+        </a>
+@empty
+    <div class="mt-2 text-center">
         <div class="card">
             <div class="card-body pt-3 pb-3">
                 <div class="empty">
@@ -59,4 +43,4 @@
             </div>
         </div>
     </div>
-@endforelse --}}
+@endforelse
