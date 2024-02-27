c7bb54834ad51d1a4b597465bb09b2c0e6c6de1c
= <=>
2024-02-27 15:30:09 +0700

    redis

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('/test_cache', Api\TestcacheController::class);
