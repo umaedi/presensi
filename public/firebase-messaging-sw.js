2d55cd6ffc77013314d16c0f1f745d8bcdacade5
= <=>
2024-02-29 14:07:12 +0700

    devkh

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -61,13 +61,12 @@ import { onBackgroundMessage } from "firebase/messaging/sw";
 const messaging = getMessaging();
 onBackgroundMessage(messaging, (payload) => {
     console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    // Customize notification here
     const notificationTitle = 'Background Message Title';
     const notificationOptions = {
         body: 'Background Message body.',
         icon: '/firebase-logo.png'
     };
 
-    // self.registration.showNotification(notificationTitle,
-    //     notificationOptions);
+    self.registration.showNotification(notificationTitle,
+        notificationOptions);
 });
