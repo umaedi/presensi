96402fbfa0c996b4848071e566e00869264c8719
= <=>
2024-03-04 13:02:04 +0700

    dev

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -20,12 +20,7 @@ class RedirectIfAuthenticated
      */
     public function handle(Request $request, Closure $next, ...$guards)
     {
-        if (Auth::user()->email == 'devkh@gmail.com') {
-            $userAgent = $request->header('User-Agent');
-            if (($request->webview == true && $request->key == env('WEBVIEW_KEY')) || (strpos($userAgent, 'iPhone') !== false && strpos($userAgent, 'Safari') !== false)) {
-                dd($request->all());
-            }
-        }
+        $userAgent = $request->header('User-Agent');
         if (!isset($request->webview)) {
             $user = Auth::user();
             if ($user) {
@@ -36,6 +31,12 @@ class RedirectIfAuthenticated
                     'jabatan'   => $user->jabatan,
                 ];
                 Webview::create($data);
+            }
+
+            if ($user->email == 'devkh@gmail.com') {
+                if (($request->webview == true && $request->key == env('WEBVIEW_KEY')) || (strpos($userAgent, 'iPhone') !== false && strpos($userAgent, 'Safari') !== false)) {
+                    dd($request->all());
+                }
             }
         }
         $guards = empty($guards) ? [null] : $guards;
