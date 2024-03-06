262d269ef26d59f7186849402c16e60a34740450
= <=>
2024-03-06 13:07:07 +0700

    devkh

--- app/Http/Controllers/Users/DashboardController.php
+++ app/Http/Controllers/Users/DashboardController.php
@@ -36,12 +36,33 @@ class DashboardController extends Controller
             return view('users.dashboard._data_table_absensi', $data);
         }
 
+        // $data['nama'] = explode(" ", auth()->user()->nama);
+        // $data['absen'] = Persensi::where('user_id', auth()->user()->id)->where('tanggal', date('Y-m-d'))->first();
+        // $data['cuti'] = Persensi::where('user_id', auth()->user()->id)->where('status', 'cuti')->count();
+        // $data['hadir'] = Persensi::where('user_id', auth()->user()->id)->whereMonth('tanggal', date('m'))->count();
+        // $data['terlambat'] = $this->presensi->Query()->where('user_id', Auth::user()->id)->whereMonth('tanggal', date('m'))->where('status', 'like', '%' . 'Terlambat' . '%')->count();
+        // $data['dl'] = Persensi::where('user_id', auth()->user()->id)->where('status', 'DL')->count();
+
+        //save cache
+        $minutes = now()->addDays(1)->diffInMinutes(now());
+
         $data['nama'] = explode(" ", auth()->user()->nama);
-        $data['absen'] = Persensi::where('user_id', auth()->user()->id)->where('tanggal', date('Y-m-d'))->first();
-        $data['cuti'] = Persensi::where('user_id', auth()->user()->id)->where('status', 'cuti')->count();
-        $data['hadir'] = Persensi::where('user_id', auth()->user()->id)->whereMonth('tanggal', date('m'))->count();
-        $data['terlambat'] = $this->presensi->Query()->where('user_id', Auth::user()->id)->whereMonth('tanggal', date('m'))->where('status', 'like', '%' . 'Terlambat' . '%')->count();
-        $data['dl'] = Persensi::where('user_id', auth()->user()->id)->where('status', 'DL')->count();
+        $data['absen'] = Cache::remember('absen_' . auth()->user()->id, $minutes, function () {
+            return Persensi::where('user_id', auth()->user()->id)->where('tanggal', date('Y-m-d'))->first();
+        });
+        $data['cuti'] = Cache::remember('cuti_' . auth()->user()->id, $minutes, function () {
+            return Persensi::where('user_id', auth()->user()->id)->where('status', 'cuti')->count();
+        });
+        $data['hadir'] = Cache::remember('hadir_' . auth()->user()->id, $minutes, function () {
+            return Persensi::where('user_id', auth()->user()->id)->whereMonth('tanggal', date('m'))->count();
+        });
+        $data['terlambat'] = Cache::remember('terlambat_' . auth()->user()->id, $minutes, function () {
+            return $this->presensi->Query()->where('user_id', Auth::user()->id)->whereMonth('tanggal', date('m'))->where('status', 'like', '%' . 'Terlambat' . '%')->count();
+        });
+        $data['dl'] = Cache::remember('dl_' . auth()->user()->id, $minutes, function () {
+            return Persensi::where('user_id', auth()->user()->id)->where('status', 'DL')->count();
+        });
+
         return view('users.dashboard.index', $data);
     }
 }
