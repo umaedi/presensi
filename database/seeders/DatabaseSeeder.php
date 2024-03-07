3fbdbd39dfe8602c1ef2019391a8b9b2c875f3b6
= <=>
2024-03-07 09:55:26 +0700

    devkh

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -83,11 +83,5 @@ class DatabaseSeeder extends Seeder
             'status'     => 'active',
             'password'  => bcrypt('devkh123'),
         ]);
-
-        \App\Models\Opd::where('id', 20)->update([
-            'nama_opd'  => 'Dinas Komunikasi dan Informatika',
-            'lat'       => '-4.5381729',
-            'long'      => '105.2213224'
-        ]);
     }
 }
