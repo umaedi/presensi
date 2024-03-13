0c3ea60acf482b76b01820db54039421bc1a2c22
= <=>
2024-03-13 11:32:01 +0700

    devkh

--- resources/views/oprator/cuti/_data_cuti.blade.php
+++ resources/views/oprator/cuti/_data_cuti.blade.php
@@ -13,8 +13,22 @@
         <tr>
             <td>{{ $table->firstItem() + $key }}</td>
             <td>{{ $tb->user->nama ?? '-' }}</td>
-            <td>{{  \Carbon\Carbon::parse($tb->tanggal_awal)->isoFormat('D MMMM Y') }}</td>
-            <td>{{ \Carbon\Carbon::parse($tb->tanggal_akhir)->isoFormat('D MMMM Y') }}</td>
+            <td>
+                {{ $date = Carbon::createFromFormat('d-m-Y', $tb->tanggal_awal) }}
+                @if ($date instanceof \Carbon\Carbon)
+                {{  \Carbon\Carbon::parse($tb->tanggal_awal)->isoFormat('D MMMM Y') }}
+                @else
+                {{ $tb->tanggal_awal }}
+                @endif
+            </td>
+            <td>
+                {{ $date = Carbon::createFromFormat('d-m-Y', $tb->tanggal_awal) }}
+                @if ($date instanceof \Carbon\Carbon)
+                {{ \Carbon\Carbon::parse($tb->tanggal_akhir)->isoFormat('D MMMM Y') }}
+                @else
+                {{ $tb->tanggal_akhir }}
+                @endif
+            </td>
             <td><a href="/oprator/cuti/show/{{ $tb->id }}" class="btn btn-primary btn-sm">Lihat</a></td>
         </tr>
         @empty
