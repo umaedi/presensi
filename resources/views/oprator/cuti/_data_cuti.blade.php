456fdb547387eb7ad740fefc3baefe74ff2b10db
= <=>
2024-03-15 14:38:38 +0700

    devkh

--- resources/views/oprator/cuti/_data_cuti.blade.php
+++ resources/views/oprator/cuti/_data_cuti.blade.php
@@ -13,8 +13,16 @@
         <tr>
             <td>{{ $table->firstItem() + $key }}</td>
             <td>{{ $tb->user->nama ?? '-' }}</td>
+            @if (strtotime($tb->tanggal_awal) !== false)
             <td>{{  \Carbon\Carbon::parse($tb->tanggal_awal)->isoFormat('D MMMM Y')  ?? ''}}</td>
+            @else
+            <td>{{ $tb->tanggal_awal }}</td>
+            @endif
+            @if (($tb->tanggal_akhir) !== false)
             <td>{{ \Carbon\Carbon::parse($tb->tanggal_akhir)->isoFormat('D MMMM Y') ?? ''}}</td>
+            @else
+            <td>{{ $tb->tanggal_akhir }}</td>
+            @endif
             <td><a href="/oprator/cuti/show/{{ $tb->id }}" class="btn btn-primary btn-sm">Lihat</a></td>
         </tr>
         @empty
