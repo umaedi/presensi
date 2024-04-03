9ffdf08b710cccd9d045d3a97b96350760e7bfcb
umaedi <humaedi.kh.99@gmail.com>
2024-04-03 15:03:32 +0700

    devkh

--- resources/views/pages/survey.blade.php
+++ resources/views/pages/survey.blade.php
@@ -2,14 +2,18 @@
 @section('content')
     <div id="appCapsule">
         <div>
-            <iframe id="myIframe" src="https://docs.google.com/forms/d/e/1FAIpQLSdKodnZwD4Nr5vlICNu2UBCyS0dbvMo37AEMJD3EIvHG4aEtQ/viewform?embedded=true" width="100%" height="11941" frameborder="0" marginheight="0" marginwidth="0">Memuat…</iframe>
+            <iframe id="googleForm" src="https://docs.google.com/forms/d/e/1FAIpQLSdKodnZwD4Nr5vlICNu2UBCyS0dbvMo37AEMJD3EIvHG4aEtQ/viewform?embedded=true" width="100%" height="11941" frameborder="0" marginheight="0" marginwidth="0">Memuat…</iframe>
         </div>
     </div>
     <script>
-         let btn = document.querySelector('.l4V7wb');
-         btn.addEventListener('click', function() {
-            // Mengarahkan pengguna ke '/user/dashboard' saat tombol diklik
-            window.location.href = '/user/dashboard';
-        });
-    </script>
+        const iframe = document.getElementById('googleForm');
+        iframe.onload = function() {
+          const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+          const submitButton = iframeDocument.querySelector('.freebirdFormviewerViewNavigationSubmitButton');
+          submitButton.addEventListener('click', function() {
+            console.log('Submit button clicked!');
+            // Lakukan sesuatu ketika tombol submit ditekan
+          });
+        };
+      </script>
 @endsection
