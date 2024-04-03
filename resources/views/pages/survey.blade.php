2d7caeed7d17d7567b469d5c0d44cf5dd0cbda4b
umaedi <humaedi.kh.99@gmail.com>
2024-04-03 14:08:06 +0700

    devkh

--- resources/views/pages/survey.blade.php
+++ resources/views/pages/survey.blade.php
@@ -4,5 +4,6 @@
         <div>
             <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdKodnZwD4Nr5vlICNu2UBCyS0dbvMo37AEMJD3EIvHG4aEtQ/viewform?embedded=true" width="100%" height="11941" frameborder="0" marginheight="0" marginwidth="0">Memuat…</iframe>
         </div>
+        <a href="/user/dashboard" class="btn btn-primary">KEMBALI</button>
     </div>
 @endsection
