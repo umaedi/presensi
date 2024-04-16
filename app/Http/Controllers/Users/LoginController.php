de6a91adbf8d262134c4e1762bf011c2546b9769
umaedi <humaedi.kh.99@gmail.com>
2024-04-16 12:25:29 +0700

    devkh

--- app/Http/Controllers/Users/LoginController.php
+++ app/Http/Controllers/Users/LoginController.php
@@ -10,7 +10,7 @@ class LoginController extends Controller
 {
     public function __invoke(Request $request)
     {
-        Cache::put('web_token', $request->key, now()->addDays(90));
+        Cache::put('web_token', $request->key, now()->addHour());
         $data['title'] = 'Login';
         return view('users.login.index', $data);
     }
