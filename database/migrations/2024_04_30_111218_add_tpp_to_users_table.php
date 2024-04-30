1e50ccc9dbf05225b7bf33c30aa9e287479b1d7f
umaedi <humaedi.kh.99@gmail.com>
2024-04-30 15:31:27 +0700

    devkh

--- database/migrations/2024_04_30_111218_add_tpp_to_users_table.php
+++ database/migrations/2024_04_30_111218_add_tpp_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('tpp')->default('-');
+            $table->string('tpp')->default('0');
         });
     }
 
